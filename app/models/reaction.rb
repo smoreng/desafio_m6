11491350f3fca3bf7d1738dba13d64f66d4e21a1
“Sofia <sofimguina@gmail.com>
2024-09-19 18:09:00 -0300

    Se valida funcionamiento de controladores

---
+++ app/models/reaction.rb
@@ -0,0 +1,9 @@
+class Reaction < ApplicationRecord
+  belongs_to :publication, optional: true
+  belongs_to :user
+  belongs_to :comment, optional: true
+  
+  Kinds = %w[like dislike not_interested neutral].freeze
+  KindsSpanish = {"like" => "Me gusta", "dislike" => "No me gusta",
+  "not_interested" => "No me interesa", "neutral" => "Neutral"}.freeze
+end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+until Publication.count == 2 do
+    Publication.create(title: Faker::Book.title,
+                    description: Faker::Lorem.paragraph_by_chars(number: 200, supplemental: false),
+                    when_went: Faker::Date.between(from: 10.years.ago, to: Date.today))
+end
